b5c629d60d38fc8ba6afc8f89a72f8da0c8fcaff
Ryo <ryo-icy@users.noreply.github.com>
2025-11-15 03:48:30 +0900

    commit

---
+++ script/setup.ps1
@@ -0,0 +1,6 @@
+kubectl apply -f ./system/gitea.yaml
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | % { [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($_)) }
